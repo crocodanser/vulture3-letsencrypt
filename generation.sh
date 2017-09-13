f52bbdd10b97b4b4568fa0b54ba8051ece0ab751
crocodanser <crocodanser@mass-n.com>
2017-09-13 18:22:03 +0200

    Update vulture's database

--- generation.sh
+++ generation.sh
@@ -3,6 +3,9 @@
 CHALLENGE_PATH="/home/vlt-sys/Engine/static/.well-known/acme-challenge/"
 PATH_CERT_VULTURE="/home/vlt-sys/Engine/conf/"
 PATH_CERT_ACME="/usr/local/etc/ssl/acme/"
+TargetDB="vulture"
+TargetCol="s_s_l_certificate"
+TargetField="cn"
 
 domaine=$1
 
@@ -62,9 +65,33 @@ do
         cat $PATH_CERT_ACME$domaine"/cert.pem" > $certfile
         cat $PATH_CERT_ACME$domaine"/chain.pem" >> $certfile
         echo -e "Le fichier $certfile du sous-domaine ${RED}$domaine${NC} a bien ete mis a jour"
+		
+		# Modification du certificat dans la base de données MongoDB :
+		certcontenu=`awk 'NF {sub(/\r/, ""); printf "%s",$0;}' "$PATH_CERT_ACME""$domaine""/cert.pem"`
+		certexpdate=$(openssl x509 -text -noout -enddate -in "$PATH_CERT_ACME""$domaine""/cert.pem" | grep notAfter | cut -f2 -d=)
+
+		dateformat=$(/bin/date -j -f "%b %d %T %Y %Z" "$certexpdate" "+%Y-%m-%d %H:%M:%S")
+
+		certexpdate="$dateformat+00:00"
+		
+		certfrom=$(openssl x509 -text -noout -dates -in "$PATH_CERT_ACME""$domaine""/cert.pem" | grep notBefore | cut -f2 -d=)
+
+        dateformat=$(/bin/date -j -f "%b %d %T %Y %Z" "$certfrom" "+%Y-%m-%d %H:%M:%S")
+
+        certfrom="$dateformat+00:00"
+
+		exp="db.s_s_l_certificate.update({\"cn\" : \""$domaine"\",\"issuer\" : \"/C=US/O=Let"\'"s Encrypt/CN=Let"\'"s Encrypt Authority X3\" }, {\$set : {\"cert\" : \""$certcontenu"\",\"validtill\" : \""$certexpdate"\",\"validfrom\" : \""$certfrom"\"}})"
+		echo -e $exp > test.js
+		mongo --ssl `hostname`:9091/$TargetDB --sslPEMKeyFile /var/db/mongodb/mongod.pem --sslCAFile /var/db/mongodb/ca.pem < test.js
+		
+
+		
+		
  fi
 
 done
+
+echo /dev/null > test.js
 
 # Recharger les interfaces HTTPS
 for ii in `ls -1 "$PATH_CERT_VULTURE"*"-443.conf"`
